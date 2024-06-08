d02e765ef2ce43025b5fb8e55e973c951aa95d40
shabd patel <shabdpatel0@gmail.com>
2024-06-08 09:48:57 +0530

    add scholarship

--- src/components/scholarship/AddScholarship.jsx
+++ src/components/scholarship/AddScholarship.jsx
@@ -1,10 +1,9 @@
-// AddScholarship.js
 import React from 'react';
 
 const AddScholarship = () => {
     return (
         <div>
-            <h1>Add Scholarship Page</h1>
+            <h1>Add Scholarship</h1>
             <p>Form to add a new scholarship...</p>
         </div>
     );
