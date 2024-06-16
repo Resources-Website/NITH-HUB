9ef43370e42843f3622e39673b47e3bd3e20c0b7
shabd patel <shabdpatel0@gmail.com>
2024-06-16 15:21:53 +0530

    env issue

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+require('dotenv').config(); // Add this line
 
 const app = express();
 const port = process.env.PORT || 5000;
