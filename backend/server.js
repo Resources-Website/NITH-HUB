fb94168397c3d1579594b42a6d889acdedb56242
shabd patel <shabdpatel0@gmail.com>
2024-06-16 11:14:40 +0530

    Updated IP adress in mongo altas

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://22bph040:eb8APetEwYQUR8V4@cluster0.o4y2lbm.mongodb.net/Scholarships?retryWrites=true&w=majority&appName=Cluster0', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://22bph040:eb8APetEwYQUR8V4@cluster0.o4y2lbm.mongodb.net/Scholarships?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connection established'))
     .catch(error => console.error('MongoDB connection error:', error));
 
